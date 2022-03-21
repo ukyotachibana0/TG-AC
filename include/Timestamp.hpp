378cd92010479467bce64df24cfc9176260ea1d4
ukyotachibana0 <3298825551@qq.com>
2022-03-21 16:45:38 +0800

    genTimestamp with window set

--- include/Timestamp.hpp
+++ include/Timestamp.hpp
@@ -32,6 +32,30 @@ public:
         int_t bound = max_timestamp - min_timestamp;
         return min_timestamp + rand.nextInt(bound);
     }
+
+    int_t genTimestamp (int_t mit, int_t mat) {
+        int_t bound = std::min(max_timestamp, mat) - std::max(min_timestamp, mit);
+        return min_timestamp + rand.nextInt(bound);
+    }
+
+    int_t genTimestamp (const std::vector<std::vector<int_t>>& window) {
+        int_t ans = 0;
+        // gen mapped ans
+        int_t bound = 0, n = window.size();
+        for (auto& win : window)
+            bound += win[1] - win[0];
+        int_t ans_map = rand.nextInt(bound);
+        // gen original ans
+        int_t last_sum = 0, sum = window.front()[1] - window.front()[0];
+        for (int_t i = 1; i < n; i++) {
+            if (ans_map <= sum)
+                return (ans_map - last_sum) + window[i - 1][0];
+
+            last_sum = sum;
+            sum += window[i][1] - window[i][0];
+        }
+        return 0;
+    }
 }; //! class Timestamp
 
 } //! namespace fastsgg
